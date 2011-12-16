4d112b95cab346d712c2f70064120dba0d493885
Your Name <james@r0bertson.co.uk>
2011-12-16 02:23:07 +0000

    1st commit

---
+++ lib/wiimagickcam.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+# file: wiimagickcam.rb
+
+require 'simple_wiimote'
+require 'magickcam'
+
+class WiiMagickCam
+
+  def self.start(raw_filename="webcam%03d.jpg")
+
+    mcam = MagickCam.new
+    swm = SimpleWiimote.new
+    i = 1
+    
+    swm.led = 1
+    puts 'press button B (trigger) to take a webcam snapshot'
+
+    swm.events[WiiMote::BTN_B] = -> {
+      swm.led = 12
+
+      filename = raw_filename  % i.to_s
+      mcam.snapshot(filename)
+      i += 1
+      swm.led = 1
+    }
+
+    swm.activate # deactivate by pressing button A
+    swm.led = 0
+    swm.close
+  end
+
+end
+
---
+++ wiimagickcam.gemspec
@@ -0,0 +1,6 @@
+Gem::Specification.new do |s|
+  s.name = 'wiimagickcam'
+  s.version = '0.1.0'
+  s.summary = 'wiimagickcam'
+  s.files = Dir['lib/**/*.rb']
+end
